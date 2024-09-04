fd6823ac30757b8e83dfdd584fe512452dbee097
Sacha Wharton <32266273+sachajw@users.noreply.github.com>
2024-09-04 22:09:27 +0200

    🛠 NEW: svelte config

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<!-- <script>
+    let name = 'Svelte'
+    let src = 'https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fynt5bho2y1xl32q2vkku.png';
+    let string = 'Some <strong>string</strong> with html';
+</script>
+
+<img {src} alt="Monkey beats {name}" />
+<h1>Hello {name.toLowerCase()}!</h1>
+<p>{@html string}</p>
+
+<style>
+    h1 {
+        color: red
+    }
+</style> -->
+
+<script>
+    import Counter from './lib/Counter.svelte';
+</script>
